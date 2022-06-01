91de2f5e9391e46ad2f5ed1b2819730d80350cd6
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-06-01 15:04:54 +0900

    style: ベタ書きよる UI の作成

--- src/components/news/NewsCard/index.tsx
+++ src/components/news/NewsCard/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import 'src/styles/NewsCard.css';
 
 const NewsCard = () => (
-  <div className="wrapper">
-    <div className="boxContainer">
-      <p>2020/12/23</p>
-      <p>重要</p>
-      <p>新商品</p>
+  <div className="newsCard">
+    <div className="dateAndLabel">
+      <p className="date">2020/12/23</p>
+      <p className="labelImportant">重要</p>
+      <p className="labelNewItem">新商品</p>
     </div>
     <p>
       テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
