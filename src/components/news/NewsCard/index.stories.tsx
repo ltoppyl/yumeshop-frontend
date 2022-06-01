caa0d3e3263c56993f159e5eb53b1c2952038bb9
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-06-01 17:00:44 +0900

    feat: お知らせのタイトルの追加

--- src/components/news/NewsCard/index.stories.tsx
+++ src/components/news/NewsCard/index.stories.tsx
@@ -6,14 +6,18 @@ export default {
   title: 'news/NewsCard',
 } as Meta;
 
-const Template: Story = ({ date, tagList, mainText }) => (
-  <NewsCard date={date} tagList={tagList} mainText={mainText} />
+const Template: Story = ({ date, tagList, newsTitle, mainText }) => (
+  <NewsCard
+    date={date}
+    tagList={tagList}
+    newsTitle={newsTitle}
+    mainText={mainText}
+  />
 );
 
 export const newsCard = Template.bind({});
 newsCard.args = {
   date: '2017-07-21T17:32:28Z',
-  mainText: 'テキスト入力の初期値 (StoreBook から上書き可能)',
   tagList: [
     {
       id: '57c3ff77-d8bd-41bb-86e3-4526e1b2186c',
@@ -28,4 +32,7 @@ newsCard.args = {
       tag_group: 'information',
     },
   ],
+  newsTitle: 'メンテナンスのお知らせ',
+  mainText:
+    '本日の19:00からメンテナンスのため、一時間ほどのサービス停止を予定しています。\n再開しだいメールにてアナウンスさせていただきます。\nご迷惑をおかけしますが、ご理解ご協力のほどをよろしくお願いいたします。',
 };
--- src/components/news/NewsCard/index.tsx
+++ src/components/news/NewsCard/index.tsx
@@ -4,10 +4,11 @@ import 'src/styles/NewsCard.css';
 type Props = {
   date: string;
   tagList: string[];
+  newsTitle: string;
   mainText: string;
 };
 
-const NewsCard = ({ date, tagList, mainText }: Props) => (
+const NewsCard = ({ date, tagList, newsTitle, mainText }: Props) => (
   <div className="newsCard">
     <div className="dateAndTag">
       <p className="date">{date.slice(0, 10)}</p>
@@ -15,7 +16,7 @@ const NewsCard = ({ date, tagList, mainText }: Props) => (
         // XXX: tag.### に対して警告が出ている (Property '###' does not exist on type 'string'.)
         <p
           key={tag.id}
-          // FIXME: NewsCard.css に書き出したい (変数の扱いが分からなかった)
+          // TODO: NewsCard.css に書き出したい (変数の扱いが分からなかった)
           style={{
             color: 'white',
             background: tag.color,
@@ -25,6 +26,7 @@ const NewsCard = ({ date, tagList, mainText }: Props) => (
         </p>
       ))}
     </div>
+    <p>{newsTitle}</p>
     <p>{mainText}</p>
   </div>
 );
