8076b60c50981320df3fc6f2d65ec9489b03ea13
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-06-01 14:42:11 +0900

    style: ベタ書きによるテキストの配置

--- src/components/news/NewsCard/index.tsx
+++ src/components/news/NewsCard/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import 'src/styles/NewsCard.css';
 
-const NewsCard = () => <p>New File</p>;
+const NewsCard = () => (
+  <div className="wrapper">
+    <div className="boxContainer">
+      <p>2020/12/23</p>
+      <p>重要</p>
+      <p>新商品</p>
+    </div>
+    <p>
+      テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+    </p>
+  </div>
+);
 
 export default NewsCard;
