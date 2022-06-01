6327cd29f66d7ce22258dbed1e5b9adfaec01135
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-06-01 16:47:30 +0900

    feat: データを変数に置き換え
    API から取得するデータに対応させるため

--- src/components/news/NewsCard/index.stories.tsx
+++ src/components/news/NewsCard/index.stories.tsx
@@ -6,6 +6,26 @@ export default {
   title: 'news/NewsCard',
 } as Meta;
 
-const Template: Story = () => <NewsCard />;
+const Template: Story = ({ date, tagList, mainText }) => (
+  <NewsCard date={date} tagList={tagList} mainText={mainText} />
+);
 
-export const footer = Template.bind({});
+export const newsCard = Template.bind({});
+newsCard.args = {
+  date: '2017-07-21T17:32:28Z',
+  mainText: 'テキスト入力の初期値 (StoreBook から上書き可能)',
+  tagList: [
+    {
+      id: '57c3ff77-d8bd-41bb-86e3-4526e1b2186c',
+      name: '重要',
+      color: 'red',
+      tag_group: 'information',
+    },
+    {
+      id: '57c3ff77-d8bd-41bb-86e3-4526e1b2186d',
+      name: 'メンテナンス',
+      color: 'yellow',
+      tag_group: 'information',
+    },
+  ],
+};
--- src/components/news/NewsCard/index.tsx
+++ src/components/news/NewsCard/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import 'src/styles/NewsCard.css';
 
-const NewsCard = () => (
+type Props = {
+  date: string;
+  tagList: string[];
+  mainText: string;
+};
+
+const NewsCard = ({ date, tagList, mainText }: Props) => (
   <div className="newsCard">
-    <div className="dateAndLabel">
-      <p className="date">2020/12/23</p>
-      <p className="labelImportant">重要</p>
-      <p className="labelNewItem">新商品</p>
+    <div className="dateAndTag">
+      <p className="date">{date.slice(0, 10)}</p>
+      {tagList.map((tag) => (
+        // XXX: tag.### に対して警告が出ている (Property '###' does not exist on type 'string'.)
+        <p
+          key={tag.id}
+          // FIXME: NewsCard.css に書き出したい (変数の扱いが分からなかった)
+          style={{
+            color: 'white',
+            background: tag.color,
+          }}
+        >
+          {tag.name}
+        </p>
+      ))}
     </div>
-    <p>
-      テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-    </p>
+    <p>{mainText}</p>
   </div>
 );
 
