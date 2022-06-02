f70f454bf1423acd11aed05b5d0fff28610e0480
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-06-02 15:18:31 +0900

    refactor: 変数名等の細かい修正

--- src/components/common/card/index.tsx
+++ src/components/common/card/index.tsx
@@ -13,11 +13,11 @@ const Card = ({ imageUrl, cardName }: Props) => {
   const isWide = useMediaLayout({ minWidth: '1000px' });
   const width = isWide ? 244 : 200;
   const height = isWide ? 244 : 200;
-  const classDevice = isWide ? 'pc' : 'sp';
+  const deviceType = isWide ? 'pc' : 'sp';
 
   return (
-    <div className={`card ${classDevice}`}>
-      <Image width={width} height={height} src={imageUrl} alt="Image" />
+    <div className={`card ${deviceType}`}>
+      <Image width={width} height={height} src={imageUrl} alt="cardName" />
       <p>{cardName}</p>
     </div>
   );
