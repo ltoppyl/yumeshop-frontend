270c492145e60175b0b3259f30f05268c76fc31e
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-06-02 09:06:26 +0900

    refactor: 表示される日付のフォーマットを変更

--- src/components/news/NewsCard/index.tsx
+++ src/components/news/NewsCard/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import dayjs from 'dayjs';
+
 import 'src/styles/NewsCard.css';
 
 type tagProps = {
@@ -17,7 +19,9 @@ type Props = {
 const NewsCard = ({ date, tagList, newsTitle, mainText }: Props) => (
   <div className="news-card">
     <div className="news-card__data-and-tag">
-      <p className="news-card__data-and-tag__date">{date.slice(0, 10)}</p>
+      <p className="news-card__data-and-tag__date">
+        {dayjs(date.slice(0, 10)).format('YYYY/MM/DD')}
+      </p>
       {tagList.map((tag) => (
         <>
           {tag.color === 'red' && (
