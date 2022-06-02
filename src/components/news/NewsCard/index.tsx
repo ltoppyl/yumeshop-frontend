a5e41a6065ee6bd1d259040c2d0b4246b5d4d21a
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-06-02 17:26:26 +0900

    refactor: お知らせカードの type の参照元の変更

--- src/components/news/NewsCard/index.tsx
+++ src/components/news/NewsCard/index.tsx
@@ -2,21 +2,9 @@ import React from 'react';
 import dayjs from 'dayjs';
 
 import 'src/styles/NewsCard.css';
+import { NewsCardDataType } from 'src/types/type';
 
-type tagProps = {
-  id: string;
-  color: 'red' | 'yellow' | 'orange';
-  name: string;
-};
-
-type Props = {
-  date: string;
-  tagList: tagProps[];
-  newsTitle: string;
-  mainText: string;
-};
-
-const NewsCard = ({ date, tagList, newsTitle, mainText }: Props) => (
+const NewsCard = ({ date, tagList, newsTitle, mainText }: NewsCardDataType) => (
   <div className="news-card">
     <div className="news-card__data-and-tag">
       <p className="news-card__data-and-tag__date">
