2255562a04f560e9bc23ab235306326bd235fd6f
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-06-02 18:16:14 +0900

    refactor: お知らせカード間のマージンの設定場所の変更

--- src/components/news/NewsCardList/index.tsx
+++ src/components/news/NewsCardList/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 import { NewsCardDataType } from 'src/types/type';
 import NewsCard from '../NewsCard';
+import 'src/styles/NewsCardList.css';
 
 export type NewsCardListProps = {
   newsDataList: NewsCardDataType[];
 };
 
 const NewCardList = ({ newsDataList }: NewsCardListProps) => (
-  <>
+  <div className="news-card-list">
     {newsDataList.map((newsData: NewsCardDataType) => (
       <NewsCard {...newsData} />
     ))}
-  </>
+  </div>
 );
 export default NewCardList;
